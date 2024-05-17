9f7916acc9048fea2a74d10201d33e0a4e0b3a14
AliFanaei <136746812+4lifanaei@users.noreply.github.com>
2024-05-18 01:50:43 +0330

    Update main.go
    quick sort first function

--- main.go
+++ main.go
@@ -7,6 +7,28 @@ import (
 	"time"
 )
 
+func quickSort(arr []int, low, high int) {
+	if low < high {
+		pi := partition(arr, low, high)
+		quickSort(arr, low, pi-1)
+		quickSort(arr, pi+1, high)
+	}
+}
+
+func partition(arr []int, low, high int) int {
+	pivot := arr[high]
+	i := low - 1
+
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return i + 1
+}
+
 func main() {
 	var flag bool = false
 	//creating a loop for repeating the options
@@ -41,6 +63,10 @@ func main() {
 			fmt.Println()
 
 		case 2:
+			arr := []int{10, 7, 8, 9, 1, 5}
+			n := len(arr)
+			quickSort(arr, 0, n-1)
+			fmt.Println("Sorted array:", arr)
 		case 3:
 			flag = true
 			fmt.Println("you are logged out.")
